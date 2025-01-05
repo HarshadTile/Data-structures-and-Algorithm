3cf785a98175a2b91e0f4e379559b1657378bf4b
Harshad Tile <tileharshad@gmail.com>
2025-01-05 13:50:48 +0530

    This is the example of the References

---
+++ A4References.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+    int a =10;
+    int &r = a ; 
+
+    cout << a << endl;
+    cout << r << endl;
+
+    
+
+
+}
