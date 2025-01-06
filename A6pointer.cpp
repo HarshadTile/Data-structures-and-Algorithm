12f8764f25b33128114987db0e98b9dbc45144b6
Harshad Tile <tileharshad@gmail.com>
2025-01-06 08:30:23 +0530

    This is simple program to access the address of r into the pointer p.

---
+++ A6pointer.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+struct Rectangle{
+    int length;
+    int breadth;
+};
+
+int main(){
+    Rectangle r ={10,5};
+    cout<<r.length<<endl;
+    cout<<r.breadth<<endl;
+
+    Rectangle *p = &r;
+    cout<<p->length<<endl;
+    cout<<p->breadth<<endl;
+
+    
+
+
+
+}
