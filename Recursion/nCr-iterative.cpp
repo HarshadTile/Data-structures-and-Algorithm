462dbd656c5bd471ad45f35f8247c11b09f40848
Harshad Tile <tileharshad@gmail.com>
2025-01-30 12:50:37 +0530

    Modified

--- Recursion/nCr-iterative.cpp
+++ Recursion/nCr-iterative.cpp
@@ -16,7 +16,14 @@ int nCr(int n,int r){
     return num/den;
 }
 
+int NCR(int n,int r){
+    if(n==r || r==0)
+        return 1;
+    return NCR(n-1,r-1)+NCR(n-1,r);
+}
+
 int main(){
     printf("%d",nCr(4,2));
+    printf("%d",NCR(4,2));
     return 0;
 }
